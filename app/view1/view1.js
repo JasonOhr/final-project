287ebdddc5a230224fc03acdd28651168136cc2b
Jason Ohr <jason.ohr@gmail.com>
2015-07-08 15:38:21 -0400

    I put in place ingredient.json that will load to local storage that has contains all of the ingredients 1. I have it working and searching this json file, but I need to do some tweaking to only show a list once a few letters have been typed in.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -6,6 +6,9 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/ingredients', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
+  }).when('/ingredients/:ndbno', {
+    templateUrl: 'view1/ingredient.html',
+    controller: 'IngredientReportCtrl'
   });
 }])
 
@@ -15,11 +18,54 @@ config(['$routeProvider', function($routeProvider) {
         $scope.nutrition = JSON.parse(localData);
         console.log('in local', $scope.nutrition);
       }else{
-        $http.get('http://api.nal.usda.gov/ndb/list?max=600&id=16020&format=json&lt=f&sort=n&api_key=z4jl046RdF4ydQqwBhipZbHkjsrKP27W94A5eIyf').success(function(data) {
+        $http.get('ingredients/ingredients.json').success(function(data) {
           localStorage.setItem('nutritionData',JSON.stringify(data));
           $scope.nutrition = data;
           console.log($scope.nutrition);
         });
-      }
+      };
+        //$http.get('https://api.parse.com/1/classes/food_des?limit=1000',{
+        //    headers:{
+        //        'X-Parse-Application-Id': '6eWfrF9o99R8oPUNvFW6mXu6iJVoBzMS0c3dMZiu',
+        //        'X-Parse-REST-API-Key':'HSHbAZxn8igmoF6wpVOQ7QfoQhKeekL4IJguGNbS',
+        //    }
+        //}).success(function(data){
+        //    console.log('first: ',data);
+        //    $http.get('https://api.parse.com/1/classes/food_des?limit=1000&skip=1000',{
+        //        headers:{
+        //            'X-Parse-Application-Id': '6eWfrF9o99R8oPUNvFW6mXu6iJVoBzMS0c3dMZiu',
+        //            'X-Parse-REST-API-Key':'HSHbAZxn8igmoF6wpVOQ7QfoQhKeekL4IJguGNbS',
+        //        }
+        //    }).success(function(data){
+        //        console.log('second: ',data);
+        //    })
+        //});
 
-}]);
+}])
+    .controller('IngredientReportCtrl', ['$scope', '$http', '$routeParams', 'returnPercentage',function($scope,$http,$routeParams, returnPercentage){
+      $http.get('http://api.nal.usda.gov/ndb/reports/?ndbno='+ $routeParams.ndbno +'&type=f&format=json&api_key=z4jl046RdF4ydQqwBhipZbHkjsrKP27W94A5eIyf').success(function(data){
+          $scope.ingredientReport = data;
+          var nutrients =   $scope.ingredientReport.report.food.nutrients;
+          var carbNuts = [208,291,209,269,210,211,212,213,214,289];
+          var proteinNuts = [203,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,521];
+          var vitaminNuts = [318,320,321,322,334,337,338,415,417,432,431,435,418,401];
+          //var macroNuts = [208,]
+        $scope.carbs = _.filter(nutrients, function(nutrients){
+            return _.contains(carbNuts, nutrients.nutrient_id)
+        });
+          //$scope.returnPercentage = function(n,d){return (n/d).toFixed(2) + '%';}
+            $scope.returnPercentage = function(n,d){
+
+              return returnPercentage(n,d);
+          }
+
+      });
+      //console.log($routeParams.ndbno);
+      //$scope.ingredient = $routeParams.id;
+    }])
+    .factory('returnPercentage',function(){
+
+        return function(n,d){
+          return (n/d).toFixed(2) + '%';
+        }
+    });
