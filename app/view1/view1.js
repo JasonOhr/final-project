671b5d91aa43a979d49820058240c85ce9ab09c8
Jason Ohr <jason.ohr@gmail.com>
2015-07-17 12:11:50 -0400

    I have the chart displaying dynamic data, tho I know it's a hack c'sest la vie

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -70,13 +70,15 @@ config(['$routeProvider', function($routeProvider) {
 
           $scope.ingredientReport = data;
           //console.log($scope.ingredientReport);
-          var nutrients =   $scope.ingredientReport.report.food.nutrients;//All nutrients and measuere for an ingredient
+          var nutrients =   $scope.ingredientReport.report.food.nutrients;//All nutrients and measure for an ingredient
 
-          //console.log('nuts',nutrients[0].measures);
+          console.log('nuts',nutrients);
           var preMeasure = nutrients[0].measures;
+          //var caloriesPerGram = Number((nutrients.value/100).toFixed.toFixed(2));
+            //console.log('cal',caloriesPerGram);
+          //console.log('preMeasure',preMeasure);
           var grams = {eqv: 1, label: 'g'};
           preMeasure.unshift(grams);
-          //console.log(preMeasure);
           $scope.measure = preMeasure;
           $scope.measurement = $scope.measure[0];
           $scope.convertToNumber();
@@ -86,24 +88,66 @@ config(['$routeProvider', function($routeProvider) {
           $rootScope.vitaminNuts = [318,320,321,322,334,337,338,415,417,432,431,435,418,401];
           var topNuts = {protein:208, carbs:203};
         $scope.carbs = _.filter(nutrients, function(nutrients){
-            return _.contains(carbNuts, nutrients.nutrient_id)
+            return _.contains(proteinNuts, nutrients.nutrient_id)
         });
           console.log('the carbs:',$scope.carbs);
-      $scope.vitamins =
-          //$scope.returnPercentage = function(n,d){return (n/d).toFixed(2) + '%';}
-            $scope.returnPercentage = function(n,d){
+          $scope.chartInfo = _.map($scope.carbs,function(newC){
+              return {y:newC.value, name:newC.name};
+          });
+          console.log($scope.chartInfo);
 
+
+         $scope.vitamins =
+            $scope.returnPercentage = function(n,d){
               return returnPercentage(n,d);
-          }
+          };
+
+          var nutrients = $scope.chartInfo
+          $scope.chartConfig =  {
+              options: {
+                  chart: {
+                      type: 'column'
+                  },
+                  drilldown:{
+                      series: [{
+                          id:'dudes',
+                          data:[
+                              ['little',3],['big',6],['whatever',2]
+                          ]
+                      },{
+                          id:'guys',
+                          data:[
+                              ['girly',5],['big',6],['whatever',2]
+                          ]
+                      },{
+                          id:'mofo',
+                          data:[
+                              ['week',1],['big',6],['whatever',2]
+                          ]
+                      }
+                      ]
+                  }
+              },
+              series: [
+                  { name: 'Jane',
+                      data: nutrients
+                  }
+              ],
+
+              title: {
+                  text:'Hello'
+              },
+              loading: false
+
+          };
+          console.log('measurement',$scope.measurement);
 
       });
         $scope.convertToNumber = function(){
             $scope.measureMultiplier = ($scope.measurement.eqv)/100;
 
-        }
-        console.log('measurement',$scope.measurement);
+        };
       //console.log($routeParams.ndbno);
-      //$scope.ingredient = $routeParams.id;
     }])
     .factory('returnPercentage',function(){
 
